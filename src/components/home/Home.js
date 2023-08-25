aa066395c91dc89a0500668c52ddf1d203981577
georgeowusu <gkojunior@gmail.com>
2023-08-24 22:11:18 -0400

    updated files

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Welcome from '../modules/welcome/Welcome'
-import './home.css'
 
 const Home = () => {
   return (
-    <div className='container'>
-      <Welcome/>
-    </div>
+    <>
+      <Welcome />
+    </>
   )
 }
 
