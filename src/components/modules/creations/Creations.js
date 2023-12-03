63182fd3f32dee2bf63f118a63e30236c2b63c40
georgeowusu <gkojunior@gmail.com>
2023-12-03 09:12:58 -0500

    route issues

--- src/components/modules/creations/Creations.js
+++ src/components/modules/creations/Creations.js
@@ -1,8 +1,71 @@
-import styles from './Creations.module.css'
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules'
+
+import { images } from '../utils/data'
+import './Creations.css'
 
 const Creations = () => {
-    return <div className={styles.container}>My Work
-    </div>
+	return (
+		<div className='container'>
+			<h1 className="heading">My Creation</h1>
+			<Swiper
+				effect={'coverflow'}
+				grabCursor={true}
+				centeredSlides={true}
+				loop={true}
+				slidesPerView={'auto'}
+				coverflowEffect={{
+					rotate: 0,
+					stretch: 0,
+					depth: 100,
+					modifier: 2.5,
+				}}
+				pagination={{ el: '.swiper-pagination', clickable: true }}
+				navigation={{
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+					clickable: true,
+				}}
+				modules={[EffectCoverflow, Pagination, Navigation]}
+				className='swiper_container'
+			>
+				<SwiperSlide>
+					<img src={images[0].image} alt='slide_image' width={50} />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={images[1].image} alt='slide_image' />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={images[2].image} alt='slide_image' />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={images[3].image} alt='slide_image' />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={images[4].image} alt='slide_image' />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={images[5].image} alt='slide_image' />
+				</SwiperSlide>
+
+				<div className='slider-controler'>
+					<div className='swiper-button-prev slider-arrow'>
+						<ion-icon name='arrow-back-outline'></ion-icon>
+					</div>
+					<div className='swiper-button-next slider-arrow'>
+						<ion-icon name='arrow-forward-outline'></ion-icon>
+					</div>
+					<div className='swiper-pagination'></div>
+				</div>
+			</Swiper>
+		</div>
+	)
 }
 
 export default Creations
